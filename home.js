18324e8d3735a14e4249945d5fe1f422b5c7a637
Tobias <tobimation23@gmail.com>
2025-02-24 14:14:04 +0100

    changed the whole thing to work as intended

--- home.js
+++ home.js
@@ -1,3 +1,8 @@
+/**
+ * Extract id as string from url to pokemon
+ * @param {string} pokemonUrl - a url to a pokemon from pokeApi 
+ * @returns {string}
+ */
 function getIdFromPokemon(pokemonUrl) {
     return pokemonUrl.slice(0, -1).split("/").pop()
 }
@@ -6,24 +11,23 @@ const artworkUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/spr
     let sectionElm = document.createElement("section")
     sectionElm.className = "pokelist"
 
-    fetch("/data/pokemon.json")
+    fetch("https://pokeapi.co/api/v2/pokemon")
     .then(function(response) {
         return response.json()
     }).then(
         function(data) {
-            sectionElm.innerHTML =  data.map(pokemon => `
+            sectionElm.innerHTML =  data.results.map(pokemon => `
+          
 
-    <article>
+    <article class="pokedex__pokemon-container">
+    <p class="pokemon__number-tag">#${getIdFromPokemon(pokemon.url).padStart(4, "0")}</p>
     <figure class="pokemon-figure">
     <img src="${artworkUrl}/${getIdFromPokemon(pokemon.url)}.png" alt="${pokemon.name}">
     </figure>
     <p class="pokemon-name">${pokemon.name} </p>
-    <a href="pokemon_details.html?id=${pokemon.url}" >
-          
-          More
-          
-          </a>
+    <a class="pokedex__pokemon-link" href="/pokemon_details.html?name=${pokemon.name}"></a>
     </article> 
+
     `).join("")
                 
               
